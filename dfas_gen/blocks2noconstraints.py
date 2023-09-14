a89dbbbbc85d37912246b1a6b9c96eb79dfe0c27
Jonas Goesgens <jonas.goesgens@ml.rwth-aachen.de>
2023-09-14 12:31:00 +0200

    started implemtation of new learing situation without state constraints; actions done

---
+++ dfas_gen/blocks2noconstraints.py
@@ -0,0 +1,129 @@
+num_objects = 2
+#predicates
+#handfree()
+#grabbed(x)
+#clear(x)
+#ontable(x)
+#on(x,y)
+#
+#actions:
+#get (x) Pre = ontable(x) wedge clear(x) wedge handfree()
+#Add = grabbed(x)
+#Del = ontable(x) wedge clear(x) wedge handfree()
+#
+#put (x) Pre = grabbed(x)
+#Add = ontable(x) wedge clear(x) wedge handfree()
+#Del = grabbed(x)
+#
+#unstack (x,y) Pre = onblock(x,y) wedge clear(x) wedge handfree()
+#Add = grabbed(x) wedge clear(y)
+#Del = onblock(x,y) wedge clear(x) wedge handfree()
+#
+#stack (x,y) Pre = grabbed(x) wedge clear(y)
+#Add = onblock(x,y) wedge clear(x) wedge handfree()
+#Del = grabbed(x) wedge clear(y)
+curr_pos = 0
+handfree_pos = curr_pos
+curr_pos += 1
+grabbed_pos = curr_pos
+curr_pos += num_objects
+clear_pos = curr_pos
+curr_pos += num_objects
+ontable_pos = curr_pos
+curr_pos += num_objects
+on_pos = curr_pos
+curr_pos += num_objects * num_objects
+num_nodes = (1 << curr_pos)
+#encoding bittable handfree,grabbed0,grabbed1,clear0,clear1,table0,table1,on00,on01,on10,on11
+
+
+#build flags
+get_action_flags = [[0] * 3 for i in range(num_objects)]
+for j in range(0, num_objects)
+    int pre = 0
+    pre = pre | (1 << handfree_pos)
+    pre = pre | (1 << (clear_pos + j))
+    pre = pre | (1 << (table_pos + j))
+    get_action_flags[j][0] = pre
+    int add = 0
+    add = add | (1 << (grabbed_pos + j))
+    get_action_flags[j][1] = add
+    int dele = 0
+    dele = dele | (1 << handfree_pos)
+    dele = dele | (1 << (clear_pos + j))
+    dele = dele | (1 << (table_pos + j))
+    get_action_flags[j][2] = dele
+
+#put x
+put_action_flags = [[0] * 3 for i in range(num_objects)]
+for j in range(0, num_objects)
+    int pre = 0
+    pre = pre | (1 << (grabbed_pos + j))
+    put_action_flags[j][0] = pre
+    int add = 0
+    add = add | (1 << handfree_pos)
+    add = add | (1 << (clear_pos + j))
+    add = add | (1 << (table_pos + j))
+    put_action_flags[j][1] = add
+    int dele = 0
+    dele = dele | (1 << (grabbed_pos + j))
+    put_action_flags[j][2] = dele
+
+
+#unstack x,y
+unstack_action_flags = [[0] * 3 for i in range(num_objects * num_objects)]
+for j in range(0, num_objects)
+    for k in range(0, num_objects)
+        int pre = 0
+        pre = pre | (1 << handfree_pos)
+        pre = pre | (1 << (clear_pos + j))
+        pre = pre | (1 << (on_pos + j*num_objects + k))
+        unstack_action_flags[j*num_objects+k][0] = pre
+        int add = 0
+        add = add | (1 << (grabbed_pos + j))
+        add = add | (1 << (clear_pos + k))
+        unstack_action_flags[j*num_objects+k][1] = add
+        int dele = 0
+        dele = dele | (1 << handfree_pos)
+        dele = dele | (1 << (clear_pos + j))
+        dele = dele | (1 << (on_pos + j*num_objects + k))
+        unstack_action_flags[j*num_objects+k][2] = dele
+
+
+#stack x,y
+stack_action_flags = [[0] * 3 for i in range(num_objects * num_objects)]
+for j in range(0, num_objects)
+    for k in range(0, num_objects)
+        int pre = 0
+        pre = pre | (1 << (grabbed_pos + j))
+        pre = pre | (1 << (clear_pos + k))
+        stack_action_flags[j*num_objects+k][0] = pre
+        int add = 0
+        add = add | (1 << handfree_pos)
+        add = add | (1 << (clear_pos + j))
+        add = add | (1 << (on_pos + j*num_objects + k))
+        stack_action_flags[j*num_objects+k][1] = add
+        int dele = 0
+        dele = dele | (1 << (grabbed_pos + j))
+        dele = dele | (1 << (clear_pos + k))
+        stack_action_flags[j*num_objects+k][2] = dele
+
+#build nodes
+for i in range(0, num_nodes)
+    #get x
+    for j in range(0, num_objects)
+
+    #put x
+    for j in range(0, num_objects)
+
+
+    #unstack x,y
+    for j in range(0, num_objects)
+        for k in range(0, num_objects)
+
+
+    #stack x,y
+    for j in range(0, num_objects)
+        for k in range(0, num_objects)
+
+
