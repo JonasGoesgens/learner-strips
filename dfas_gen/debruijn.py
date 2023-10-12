31b4abd9122c99be5b5a725407dc92bae696c9fb
Jonas Goesgens <jonas.goesgens@rwth-aachen.de>
2023-10-12 13:57:50 +0200

    Switched to phased debruijn

--- dfas_gen/debruijn.py
+++ dfas_gen/debruijn.py
@@ -14,15 +14,18 @@
 for num_objects in range(2,8):
     output_string = ""
     num_nodes = (1 << num_objects)
-    for node in range(0,num_nodes):
-        node_string = ""
-        target = node
-        target = ((target << 1) | (target >> num_objects - 1)) % num_nodes
-        node_string += " shuffle " + str(target)
-        target = (node & ~1) | (~node & 1)
-        target = ((target << 1) | (target >> num_objects - 1)) % num_nodes
-        node_string += " shuffleexchange" + str(node & 1) + " " + str(target)
-        output_string += str(2) + node_string + "\n"
-    output_string = "dfa " + str(num_nodes) + " -1\n" + "3 shuffle shuffleexchange0 shuffleexchange1\n" + "1 0\n" + output_string
+    for phase in range(0,num_objects):
+        for node in range(0,num_nodes):
+            node_string = ""
+            target = node
+            target = ((target << 1) | (target >> num_objects - 1)) % num_nodes
+            target = target | (((phase + 1) % num_objects ) << num_objects)
+            node_string += " shuffle " + str(target)
+            target = (node & ~1) | (~node & 1)
+            target = ((target << 1) | (target >> num_objects - 1)) % num_nodes
+            target = target | (((phase + 1) % num_objects ) << num_objects)
+            node_string += " shuffleexchange" + str(node & 1) + " " + str(target)
+            output_string += str(2) + node_string + "\n"
+    output_string = "dfa " + str(num_nodes*num_objects) + " -1\n" + "3 shuffle shuffleexchange0 shuffleexchange1\n" + "1 0\n" + output_string
     with open('debruijn' + str(num_objects) + '.dfa', 'w') as f:
         f.write(output_string)
