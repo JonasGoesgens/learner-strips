be5f3e55ee4d6d1461384cedfe1f8fe8ef6f0421
Jonas Gösgens <jonas.goesgens@rwth-aachen.de>
2023-11-14 15:13:46 +0100

    Stealined suppression argument

--- asp/scripts/make_lp_from_dfa.py
+++ asp/scripts/make_lp_from_dfa.py
@@ -42,7 +42,7 @@ def get_args():
     parser = argparse.ArgumentParser(description='Translate .dfa file (labeled directed graph) into .lp file that can be processed by clingo.')
     parser.add_argument('--debug_level', nargs=1, type=int, default=default_debug_level, help='Set debug level')
     parser.add_argument('--log_file', nargs=1, type=Path, default=default_log_file, help=f"Set log filename (default='{default_log_file}')")
-    parser.add_argument('--suppress_labels', type=bool, help='Surpress tlabel facts in target file for blackbox edges')
+    parser.add_argument('--suppress_labels', action='store_true', help='Surpress tlabel facts in target file for blackbox edges')
     parser.add_argument('dfa', type=Path, help='Filename or folder for .dfa containing state space graph')
     parser.add_argument('lp', type=Path, help='Filename or folder for output .lp file')
 
